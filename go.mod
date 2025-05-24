bb482528ce6977001bcbc9f34a001bb9790c5360
kazmaxim <kazmaxim@technologysolutionsgeorgia.ge>
2025-05-24 21:44:57 +0300

    add temporary http server launch for gcp

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/BernsteinMond/gorecengine
 go 1.23.8
 
 require (
+	github.com/caarlos0/env/v11 v11.3.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
--- src/cmd/server/config.go
+++ src/cmd/server/config.go
@@ -1,3 +1,22 @@
 package main
 
-type Config struct{}
+import (
+	"fmt"
+	"github.com/caarlos0/env/v11"
+)
+
+type Config struct {
+	InferenceServer HTTPServer `envPrefix:"INFERENCE_SERVER_"`
+}
+
+type HTTPServer struct {
+	ListenAddr string `env:"LISTEN_ADDR"`
+}
+
+func loadCfgFromEnv() (Config, error) {
+	cfg, err := env.ParseAs[Config]()
+	if err != nil {
+		return Config{}, fmt.Errorf("parse environment: %w", err)
+	}
+	return cfg, nil
+}
